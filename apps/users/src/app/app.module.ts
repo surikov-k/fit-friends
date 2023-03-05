e6bf74ea7efa6fe08b6d2db55b0d93b1af62a9f4
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2023-03-05 22:30:39 +0300

    build: Add ConfigModule to users service

--- apps/users/src/app/app.module.ts
+++ apps/users/src/app/app.module.ts
@@ -1,6 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { ENV_FILE_PATH } from './app.constants';
+import databaseConfig from './config/database.config';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+      envFilePath: ENV_FILE_PATH,
+      load: [databaseConfig],
+    })
+  ],
   controllers: [],
   providers: [],
 })
