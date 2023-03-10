14caa4857bc72ec06f8ca4cfd1d41a2f048c6157
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2023-03-10 12:24:15 +0300

    feat(core) Add fillObject helper

--- libs/core/src/lib/helpers.ts
+++ libs/core/src/lib/helpers.ts
@@ -1,3 +1,18 @@
-export function getMongoConnectionString({username, password, host, port, databaseName, authDatabase}): string {
+import { ClassConstructor, plainToInstance } from 'class-transformer';
+
+export function fillObject<T, V>(someDto: ClassConstructor<T>, plainObject: V) {
+  return plainToInstance(someDto, plainObject, {
+    excludeExtraneousValues: true,
+  });
+}
+
+export function getMongoConnectionString({
+  username,
+  password,
+  host,
+  port,
+  databaseName,
+  authDatabase,
+}): string {
   return `mongodb://${username}:${password}@${host}:${port}/${databaseName}?authSource=${authDatabase}`;
 }
