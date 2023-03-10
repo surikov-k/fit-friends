f5dceca98017df127e33e08c2a43ae926c37dcfa
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2023-03-10 12:48:58 +0300

    feat(users) Add an auth controller

--- apps/users/src/app/auth/auth.controller.ts
+++ apps/users/src/app/auth/auth.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+
+import { fillObject } from '@fit-friends/core';
+import { AuthService } from './auth.service';
+import { LoginDto, RegisterDto } from './dto';
+import { LoggedRdo, UserRdo } from './rdo';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  public async register(@Body() dto: RegisterDto) {
+    const user = await this.authService.register(dto);
+    return fillObject(UserRdo, user);
+  }
+
+  @Post('login')
+  public async login(@Body() dto: LoginDto) {
+    const user = await this.authService.verify(dto);
+    return fillObject(LoggedRdo, user);
+  }
+
+  @Get('id')
+  public async get(@Param('id') id: string) {
+    const user = await this.authService.get(id);
+    return fillObject(UserRdo, user);
+  }
+}
--- apps/users/src/app/auth/rdo/index.ts
+++ apps/users/src/app/auth/rdo/index.ts
@@ -1,1 +1,2 @@
 export * from './user.rdo';
+export * from './logged.rdo';
