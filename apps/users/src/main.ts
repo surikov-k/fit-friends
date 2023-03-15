46a0b5b7962612c078798e44afdb53fc9c030b1a
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2023-03-15 20:29:29 +0300

    ✨ feat: Add swagger setup

--- apps/users/src/main.ts
+++ apps/users/src/main.ts
@@ -5,6 +5,7 @@
 
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 import { useContainer } from 'class-validator';
@@ -14,9 +15,17 @@ async function bootstrap() {
 
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
+  const config = new DocumentBuilder()
+    .setTitle('Users service')
+    .setDescription('Users service API')
+    .setVersion('1.0')
+    .build();
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('spec', app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -24,6 +33,7 @@ async function bootstrap() {
     })
   );
 
+  const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
