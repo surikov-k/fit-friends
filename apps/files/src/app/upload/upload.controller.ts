9c394f8bea70909d6140c3bc7acfe8bed7e8ccc4
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2023-03-17 18:26:37 +0300

    feat(files): Add saveAvatarFile method to the upload controller

--- apps/files/src/app/upload/upload.controller.ts
+++ apps/files/src/app/upload/upload.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  FileTypeValidator,
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+import { UploadService } from './upload.service';
+import { UploadFile } from './upload.constants';
 
 @Controller('upload')
-export class UploadController {}
+export class UploadController {
+  uploadPath = '';
+
+  constructor(private readonly uploadService: UploadService) {}
+
+  @Post('avatar')
+  @UseInterceptors(FileInterceptor('avatar'))
+  public async saveAvatarFile(
+    @UploadedFile(new ParseFilePipe(UploadController.parseAvatarPipeOptions))
+    { filename }: Express.Multer.File
+  ) {
+    return this.uploadService.save({ filename });
+  }
+
+  private static parseAvatarPipeOptions = {
+    validators: [
+      new FileTypeValidator({ fileType: UploadFile.AVATAR_TYPE }),
+      new MaxFileSizeValidator({ maxSize: UploadFile.AVATAR_MAX_SIZE }),
+    ],
+  };
+}
