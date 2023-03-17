02e81c80eb42081fe80161fb15b704eb7bf5fa3a
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2023-03-17 15:36:50 +0300

    feat(files): Add an upload service

--- apps/files/src/app/upload/upload.service.ts
+++ apps/files/src/app/upload/upload.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { UploadRepository } from './upload.repository';
+
+import { UploadEntity } from './upload.entity';
+import { SaveUploadDto } from './dto';
 
 @Injectable()
-export class UploadService {}
+export class UploadService {
+  constructor(private readonly uploadRepository: UploadRepository) {}
+
+  public async save(upload: SaveUploadDto) {
+    return this.uploadRepository.create(
+      new UploadEntity({ ...upload, uploaderId: 'uploader' })
+    );
+  }
+}
