556eac57697b4ecfa53f7c4b521816aaef61fcc2
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2023-03-17 12:53:21 +0300

    feat(files): Import mongoose module

--- apps/files/src/app/upload/upload.module.ts
+++ apps/files/src/app/upload/upload.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { UploadController } from './upload.controller';
 import { UploadService } from './upload.service';
+import { UploadModel, UploadSchema } from './upload.model';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: UploadModel.name,
+        schema: UploadSchema,
+      },
+    ]),
+  ],
   controllers: [UploadController],
   providers: [UploadService],
 })
