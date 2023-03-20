6a779b7fef04300ae853e409cb0453c905f97d16
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2023-03-21 01:11:29 +0300

    ✨ feat(workouts): Add a workout module

--- apps/workouts/src/app/app.module.ts
+++ apps/workouts/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { WorkoutModule } from './workout/workout.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, WorkoutModule],
   controllers: [],
   providers: [],
 })
