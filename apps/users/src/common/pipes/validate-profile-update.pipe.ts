cbdc7fe92a0ac5a481b0d349c13699a1314f9386
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2023-03-20 15:23:39 +0300

    refactor(users): Refactor the profile update validation pipe

--- apps/users/src/common/pipes/validate-profile-update.pipe.ts
+++ apps/users/src/common/pipes/validate-profile-update.pipe.ts
@@ -7,24 +7,19 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { REQUEST } from '@nestjs/core';
-import { plainToInstance } from 'class-transformer';
-import { validate } from 'class-validator';
 
 @Injectable({ scope: Scope.REQUEST })
 export class ValidateProfileUpdate implements PipeTransform {
   constructor(@Inject(REQUEST) private readonly request: Request) {}
 
   async transform(value: any, metadata: ArgumentMetadata) {
-    const { metatype } = metadata;
     const role = this.request['user']['role'];
     const validationPipe = new ValidationPipe({
       whitelist: true,
       skipMissingProperties: true,
       groups: [role],
     });
-    const object = plainToInstance(metatype, value);
-    const errors = await validate(object);
-    console.log(errors);
+
     return validationPipe.transform(value, metadata);
   }
 }
