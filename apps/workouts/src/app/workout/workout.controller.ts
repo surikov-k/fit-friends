6740a74399c43a482794ac2f453ddfb1de06060e
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2023-03-21 17:27:31 +0300

    feat(workouts): Add a current user decorator to create workout endpoint

--- apps/workouts/src/app/workout/workout.controller.ts
+++ apps/workouts/src/app/workout/workout.controller.ts
@@ -1,17 +1,22 @@
+import { ApiTags } from '@nestjs/swagger';
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+
+import { CoachGuard, CurrentUserId, fillObject } from '@fit-friends/core';
 import { WorkoutService } from './workout.service';
 import { CreateWorkoutDto } from './dto';
-import { CoachGuard, fillObject } from '@fit-friends/core';
 import { WorkoutRdo } from './rdo';
 
+@ApiTags('workout')
 @Controller('workout')
 export class WorkoutController {
   constructor(private readonly workoutService: WorkoutService) {}
 
-  @UseGuards(CoachGuard)
   @Post()
-  public async create(@Body() dto: CreateWorkoutDto) {
-    const coachId = '6411b0cf332c6121f8682a0f';
+  @UseGuards(CoachGuard)
+  public async create(
+    @Body() dto: CreateWorkoutDto,
+    @CurrentUserId() coachId: string
+  ) {
     const workout = await this.workoutService.create(coachId, dto);
 
     return fillObject(WorkoutRdo, workout);
