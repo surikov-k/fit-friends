ebb455df4b66f5091c87c899464f45c34f4413b3
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2023-03-25 00:56:24 +0300

    feat(orders): Add a prisma module

--- apps/orders/src/app/app.module.ts
+++ apps/orders/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+
 import { OrderModule } from './order/order.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [OrderModule],
+  imports: [OrderModule, PrismaModule],
   controllers: [],
   providers: [],
 })
