dea1f1a7b448af248201b7317b02927954bffcb9
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2023-04-26 15:48:54 +0300

    feat(webapp): Add static files

--- apps/webapp/src/main.tsx
+++ apps/webapp/src/main.tsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-
-import App from './app/app';
+import App from './app/components/app';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
