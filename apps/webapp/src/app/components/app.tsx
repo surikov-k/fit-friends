c0fecc07d1aab1dba460ad888df337f46359d7a5
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2023-04-27 08:52:36 +0300

    feat(webapp): Add a scroll lock hook

--- apps/webapp/src/app/components/app.tsx
+++ apps/webapp/src/app/components/app.tsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import { WelcomeScreen } from '../pages/welcome-screen';
+import { AppRoute } from '../app.constants';
 
 export function App() {
-  return <WelcomeScreen />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Root} element={<WelcomeScreen />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
