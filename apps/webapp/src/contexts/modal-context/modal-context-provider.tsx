415a191e789baf861a85b2655c1e07acb45a3406
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2023-04-27 08:54:07 +0300

    feat(webapp): Add a modal context

---
+++ apps/webapp/src/contexts/modal-context/modal-context-provider.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+
+import { ModalContext } from './modal-context';
+import { useScrollLock } from '../../hooks';
+
+type ModalProviderProps = {
+  children: JSX.Element[];
+};
+
+export function ModalProvider({ children }: ModalProviderProps) {
+  const [opened, setOpened] = useState<boolean>(false);
+  const [content, setContent] = useState<JSX.Element | null>(null);
+
+  const lockScroll = useScrollLock();
+
+  const open = (content: JSX.Element) => {
+    setOpened(true);
+    setContent(content);
+    lockScroll(true);
+  };
+
+  const close = () => {
+    setOpened(false);
+    lockScroll(false);
+  };
+
+  return (
+    <ModalContext.Provider value={{ open, close }}>
+      <>
+        {children}
+        {opened && content}
+      </>
+    </ModalContext.Provider>
+  );
+}
